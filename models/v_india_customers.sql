a9c1ca746d56f3b6f9b50117646ce5cb8e5a1002
venkata Srinivas Gunda <venkatsrinivasgunda@gmail.com>
2026-01-10 00:23:31 +0000

    commit

--- models/v_india_customers.sql
+++ models/v_india_customers.sql
@@ -6,6 +6,6 @@
 }}
 
 select c_custkey,c_name,c_address,c_phone,n_name
- from dev.dev_schema.customer inner join dev.dev_schema.nation
+ from {{ source ('s1','customer')}} join {{ source ('s1','nation')}}
 on c_nationkey=n_nationkey 
 where c_nationkey=8
